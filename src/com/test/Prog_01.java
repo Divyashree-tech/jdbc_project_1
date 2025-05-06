0a9240e09edd0e0bf8691d164fc0a1e1cb49d5c1
Divyashree B <divyashree.b74@gmail.com>
2025-05-06 09:34:02 +0530

    Update Prog_01.java

--- src/com/test/Prog_01.java
+++ src/com/test/Prog_01.java
@@ -1,10 +1,13 @@
 package com.test;
-
+import java.util.Scanner;
 public class Prog_01 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Wo0rld");
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int square = number * number;
+        System.out.println("The square of " + number + " is: " + square);
+        scanner.close();	
 	}
 
 }
